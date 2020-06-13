812969c01792b17789cefcb9372e02d5c96d28d3
Bibek Panthi <bpanthi977@gmail.com>
2020-06-13 17:51:07 +0545

    unix line endings

--- example.lisp
+++ example.lisp
@@ -15,7 +15,7 @@
 (defun example-mma()
   "The https://nlopt.readthedocs.io/en/latest/NLopt_Tutorial/ problem"
   (let ((nlopt (create :nlopt_ld_mma 2)))
-	(set-lower-bounds nlopt (darray -11d0 0.001d0))
+	(set-lower-bounds nlopt (doubles (list -11d0 0.001d0)))
 	(set-min-objective nlopt (lambda (x grad nlopt)
 							   (declare (ignore nlopt))
 							   (let ((sqrtx1 (sqrt (dref x 1))))
@@ -45,4 +45,42 @@
 								   (- (expt (+ (- x0) 1) 3) x1)))
 							   1d-2)
 	(set-xtol-rel nlopt 1d-4)
-	(optimize-nlp nlopt (darray 1.234d0 5.67d0))))
+	(optimize-nlp nlopt (doubles (list 1.234d0 5.67d0)))))
+
+(defun example2 ()
+  (let ((nlopt (create :nlopt_ln_cobyla 5)))
+	(set-min-objective nlopt
+					   (lambda (x grad nlopt)
+						 (declare (ignore grad nlopt))
+						 (dreffing@ x
+						   (+ @0 @1 @2 @3 @4))))
+	(setf (lower-bounds nlopt) -10d0)
+	(setf (xtol-rel nlopt) 1d-4)
+	;; (add-equality-constraint nlopt
+	;; 						 (lambda (x grad nlopt)
+	;; 						   (declare (ignore grad nlopt))
+	;; 						   (dreffing@ x (+ @0 @1))))
+	(add-equality-mconstraint nlopt 3
+							  (lambda (x grad nlopt)
+								(declare (ignore grad nlopt))
+								(dreffing@ x
+								  (list (+ @0 (* @1 @2))
+										(- @3 (/ @4 @2))
+										(/ @1 (* @0 @1)))))
+							  0.001)
+	(optimize-nlopt nlopt 1)))
+									  
+(defmacro with-nlopt ((nlopt-var algorithm dimension &key (xtol-rel 1d-4) (xtol-abs 1d-4)) &body body)
+  `(let ((,nlopt-var (create ,algorithm ,dimension)))
+	 (setf (xtol-rel ,nlopt-var) ,xtol-rel)
+	 (setf (xtol-abs ,nlopt-var) ,xtol-abs)
+   ,@body))
+
+(defun example3 ()
+  (with-nlopt (nlopt :nlopt_ln_cobyla 1 :xtol-rel 1d-6 :xtol-abs 1d-7)
+	(set-min-objective nlopt (lambda (x grad nlopt)
+							   (declare (ignore grad nlopt))
+							   (abs (- (exp (- (dref x 0))) (exp 1)))))
+	(setf (ftol-ab))
+	(optimize-nlopt nlopt 10)))
+
